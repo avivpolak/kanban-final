8915a995ef5f73514a5ee06271221ba8f246aa30
AVIV POLAK <avivp0437@gmail.com>
2021-09-23 21:40:04 +0300

    4th TEST PASS

--- solution/index.js
+++ solution/index.js
@@ -1,20 +1,21 @@
+'use strict'
 if (!localStorage.tasks) {
     //if there is info in local storage use it , otherwise create a new, empty "tasks" & "taskIdcount"
 
-    tasks = {
+    let tasks = {
         todo: [],
         'in-progress': [],
         done: [],
     }
 
-    sendToLocal()
+    localStorage.setItem('tasks', JSON.stringify(tasks))
 }
-tasks = JSON.parse(localStorage.getItem('tasks')) //get info from local storage.
+let tasks = JSON.parse(localStorage.getItem('tasks')) //get info from local storage.
 displayElements()
 //making the bottons work
-document.getElementById('submit-add-to-do').addEventListener('click', addToDoTask)
-document.getElementById('submit-add-in-progress').addEventListener('click', addInProgressTask)
-document.getElementById('submit-add-done').addEventListener('click', addDoneTask)
+document.getElementById('submit-add-to-do').addEventListener('click', handleaddToDoTask)
+document.getElementById('submit-add-in-progress').addEventListener('click', handleaddInProgressTask)
+document.getElementById('submit-add-done').addEventListener('click', handleaddDoneTask)
 //other
 function getInputInfo(inputId) {
     //returns this input value
@@ -36,27 +37,58 @@ function addTask(taskName, state) {
     displayElements()
 }
 
-function addToDoTask() {
+function handleaddToDoTask() {
     if (document.getElementById('add-to-do-task').value === '') {
         alert('empty input')
         return null
     }
     addTask(getInputInfo('add-to-do-task'), 'todo')
 }
-function addInProgressTask() {
+function handleaddInProgressTask() {
     if (document.getElementById('add-in-progress-task').value === '') {
         alert('empty input')
         return null
     }
     addTask(getInputInfo('add-in-progress-task'), 'in-progress')
 }
-function addDoneTask() {
+function handleaddDoneTask() {
     if (document.getElementById('add-done-task').value === '') {
         alert('empty input')
         return null
     }
     addTask(getInputInfo('add-done-task'), 'done')
 }
+
+//move task
+function moveTask(taskName, target) {
+    removeTask(taskName)
+    addTask(taskName, target)
+}
+//remove
+function removeTask(taskName) {
+    let i = 0
+    for (let task of tasks.todo) {
+        if (task === taskName) {
+            tasks['todo'].splice(i, 1)
+        }
+        i++
+    }
+    i = 0
+    for (let task of tasks['in-progress']) {
+        if (task === taskName) {
+            tasks['in-progress'].splice(i, 1)
+        }
+        i++
+    }
+    i = 0
+    for (let task of tasks.done) {
+        if (task === taskName) {
+            tasks['done'].splice(i, 1)
+        }
+        i++
+    }
+}
+
 //DOM
 function createElement(tagname, children = [], classes = [], attributes, events) {
     //the most generic element builder.
@@ -146,4 +178,53 @@ function displayElements() {
     removeAllchildrens('inProgressTasks')
     removeAllchildrens('doneTasks')
     generateTasks()
+}
+//events handaling
+
+document.getElementById('toDoTasks').addEventListener('mouseover', mouseOverParent)
+document.getElementById('inProgressTasks').addEventListener('mouseover', mouseOverParent)
+document.getElementById('doneTasks').addEventListener('mouseover', mouseOverParent)
+document.getElementById('toDoTasks').addEventListener('mouseleave', mouseleaveParent)
+document.getElementById('inProgressTasks').addEventListener('mouseleave', mouseleaveParent)
+document.getElementById('doneTasks').addEventListener('mouseleave', mouseleaveParent)
+document.getElementById('toDoTasks').addEventListener('dblclick', handleDubleClick)
+document.getElementById('inProgressTasks').addEventListener('dblclick', handleDubleClick)
+document.getElementById('doneTasks').addEventListener('dblclick', handleDubleClick)
+document.getElementById('toDoTasks').addEventListener('blur', handleBlur)
+document.getElementById('inProgressTasks').addEventListener('blur', handleBlur)
+document.getElementById('doneTasks').addEventListener('blur', handleBlur)
+
+let correntTaskBelow = null
+function mouseOverParent(e) {
+    //when the mouse over parent
+    //-->update corrent element below(if its LI)
+    document.addEventListener('keydown', handleKeyDown) //start listen to keydown.
+
+    correntTaskBelow = null //an real-time updated object that indicates the the tops element below mouse
+    if (e.target.tagName === 'LI') {
+        correntTaskBelow = e.target.innerText
+    }
+}
+function handleDubleClick(event) {
+    console.log(correntTaskBelow)
+}
+function handleBlur(event) {
+    console.log(correntTaskBelow)
+}
+function handleKeyDown(keyDownEvent) {
+    keyDownEvent.preventDefault()
+    if (keyDownEvent.altKey) {
+        if (keyDownEvent.key === '1') {
+            moveTask(correntTaskBelow, 'todo')
+        }
+        if (keyDownEvent.key === '2') {
+            moveTask(correntTaskBelow, 'in-progress')
+        }
+        if (keyDownEvent.key === '3') {
+            moveTask(correntTaskBelow, 'done')
+        }
+    }
+}
+function mouseleaveParent() {
+    document.removeEventListener('keydown', handleKeyDown) //stop listen to keydown.
 }
