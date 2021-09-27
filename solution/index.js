d674144d471bdc641c077d32e7e6a71b2dc35003
AVIV POLAK <avivp0437@gmail.com>
2021-09-27 06:59:40 +0300

    works 8/8 with drag &drop!

--- solution/index.js
+++ solution/index.js
@@ -450,21 +450,30 @@ function createExtraElement(title) {
 function openExtraInfo(event) {
     //adds a relevant extra info element after the task element
     //if its exsist already, removes it.(basiclly toggiling)
-
+    setTimeout(function(){ 
+        
+        if (dblClicked === true) return
     const title = stringToKabab(event.target.firstChild.wholeText)
     if (event.target.parentElement.querySelectorAll(`[data-title-exstra~="${title}"]`)[0])
         event.target.parentElement.querySelectorAll(`[data-title-exstra~="${title}"]`)[0].remove()
     else {
         createExtraElement(title)
-    }
+    } }, 300);
 }
 
 function createTaskElement(title, state) {
     //uses createElement to creat an task elment
     //appends it to the matching <ul>
 
-    let newTaskElement = createElement('li', [title], ['task', 'draggable'], { 'data-title': stringToKabab(title) }, {})
+    let newTaskElement = createElement(
+        'li',
+        [title],
+        ['task', 'draggable'],
+        { 'data-title': stringToKabab(title) },
+        {}
+    )
     newTaskElement.addEventListener('click', openExtraInfo)
+    newTaskElement.addEventListener("mousedown",clickDrugAndDropHandler)
 
     if (state === 'todo') appendElement('toDoTasks', newTaskElement)
     if (state === 'in-progress') appendElement('inProgressTasks', newTaskElement)
@@ -597,9 +606,6 @@ function handleRemove(event) {
 }
 
 //move task by alt+(1-3) & rename by doubleclick :
-//im am truely ashemed this is the way im am doing it.
-//i have strugged it too much time and i could not find a better way(wich im sure there is),
-//but i hade to go on so this is my solution it now:
 
 let correntTaskBelow = null //an real-time updated variable indicates the task name below mouse
 let correntTaskElementBelow = null //an real-time updated object that indicates the the tops element below mouse
@@ -632,6 +638,15 @@ function handleDubleClick() {
     wasJustFocusedOldName = correntTaskElementBelow.innerText
     correntTaskElementBelow.focus()
     wasJustFocused = correntTaskElementBelow
+
+    const range = document.createRange()
+    const sel = window.getSelection()
+    const length =correntTaskElementBelow.innerText.length
+    correntTaskElementBelow.innerText=""
+    range.setStart(correntTaskElementBelow.firstChild,length)
+    range.collapse(true)
+    sel.removeAllRanges()
+    sel.addRange(range)
 }
 
 function handleBlur() {
@@ -770,6 +785,135 @@ async function saveData() {
     } else document.getElementById('errorBar').innerText = 'saved!'
 }
 
+let dblClicked = false // later used to determine if to start drag and drop
+let mouseDown = false // later used to determine if to start drag and drop
+
+function getAncestorSectionListId(liElement) {
+    return Number(liElement.closest('section').dataset.originalListId)
+}
+
+function onDragStart() {
+    return false
+}
+
+function clickDrugAndDropHandler(event) {
+    const target = event.target
+
+    event.preventDefault() // to prevent the selecting action of click
+
+    mouseDown = true
+
+    function onMouseUp(event) {
+        if (event.target === target) {
+            mouseDown = false
+        }
+    }
+
+    document.addEventListener('mouseup', onMouseUp)
+
+    // the main drag and drop section!
+    // we set a time out so the user can make a dblclick without starting to drag
+    // I found that the best time is 300 ms. microsoft says its 500 ms ;)
+
+    setTimeout(() => {
+        if (dblClicked === true || mouseDown === false) return
+
+        event.preventDefault()
+
+        target.classList.add('taskOnTheMove')
+
+        let shiftX = event.clientX - target.getBoundingClientRect().left
+        let shiftY = event.clientY - target.getBoundingClientRect().top
+
+        target.style.position = 'absolute'
+        target.style.zIndex = 1000
+        document.body.append(target)
+
+        moveAt(event.pageX, event.pageY)
+
+        // moves the task at (pageX, pageY) coordinates
+        // taking initial shifts into account
+        function moveAt(pageX, pageY) {
+            target.style.left = pageX - shiftX + 'px'
+            target.style.top = pageY - shiftY - 5 + 'px' // small adjustment for better performance
+        }
+
+        let currentDroppable = null
+
+        function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY)
+
+            target.style.display = 'none'
+            let elemBelow = document.elementFromPoint(event.clientX, event.clientY)
+            target.style.display = ''
+
+            // mousemove events may trigger out of the window (when the ball is dragged off-screen)
+            // if clientX/clientY are out of the window, then elementFromPoint returns null
+            if (!elemBelow) return
+
+            // potential droppable are labeled with the class "droppable" (can be other logic)
+            let droppableBelow = elemBelow.closest('.droppable')
+
+            if (currentDroppable != droppableBelow) {
+                // we're flying in or out...
+                // note: both values can be null
+                //   currentDroppable=null if we were not over a droppable before this event (e.g over an empty space)
+                //   droppableBelow=null if we're not over a droppable now, during this event
+
+                if (currentDroppable) {
+                    // the logic to process "flying out" of the droppable (remove highlight)
+                    leaveDroppable(currentDroppable)
+                }
+                currentDroppable = droppableBelow
+                if (currentDroppable) {
+                    // the logic to process "flying in" of the droppable
+                    enterDroppable(currentDroppable)
+                }
+            }
+        }
+
+        // move the task on mousemove
+        document.addEventListener('mousemove', onMouseMove)
+
+        // drop the task, remove unneeded handlers
+
+        // drop the task, remove unneeded handlers
+        target.onmouseup = function () {
+            if (currentDroppable) {
+                moveTask(kababToString(target.dataset.title), currentDroppable.dataset.state)
+                currentDroppable.classList.remove('waitingForDrop')
+                document.removeEventListener('mousemove', onMouseMove)
+                target.onmouseup = null
+                target.remove()
+                displayElements()
+            } else {
+                document.removeEventListener('mousemove', onMouseMove)
+                target.onmouseup = null
+                target.remove()
+                displayElements()
+                return
+            }
+        }
+    }, 300)
+}
+
+let aboveDroppable = false
+
+function enterDroppable(droppableElement) {
+    aboveDroppable = true
+
+    if (droppableElement.tagName === 'LI') {
+        droppableElement.classList.add('below-drag')
+    }
+}
+
+function leaveDroppable(droppableElement) {
+    aboveDroppable = false
+
+    if (droppableElement.tagName === 'LI') {
+        droppableElement.classList.remove('below-drag')
+    }
+}
 // The Gauge-- not my writing! its imported and modified to my needs
 
 function Gauge(el) {
