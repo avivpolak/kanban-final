bce61b53d242b15b9d4eac2ceb9e6165fce0ab8e
AVIV POLAK <avivp0437@gmail.com>
2021-10-12 19:21:57 +0300

    start dividing

--- solution/index.js
+++ solution/index.js
@@ -1,58 +1,9 @@
 'use strict'
 import style from './styles.css'
+import tasks from "./data/dataStructures"
+import taskExtraInfo from "./data/dataStructures"
 
-//
 
-//DATA STRUCTURES-
-
-//***"tasks" :
-//will handle all of the tasks data structure and the work with the local storage and api */
-//each array contains string's- the titles of the tasks.
-//EVERY TITLE IS UNIQE! - the solution blocks the user from adding 2 same titles.
-
-//
-
-let tasks = {
-    todo: [],
-    'in-progress': [],
-    done: [],
-}
-
-//***"taskExtraInfo":
-//will handle all of the extra tasks data structure and the work with local storage
-//EACH "tasks" TITLE IS A KEY TO ITS DATA IN "taskExtraInfo".
-//FOR EXSAMPLE:
-//
-//   "laundry": {
-
-//      description: "I have to do laundry because
-//      it's already hard fo…et into a room with too
-//      many clothes on the floor",
-
-//      priority: '7',
-
-//      deadline: '2021-09-27',
-
-//      timeEstimated: '1',
-
-//      parentTask: 'Housework'}
-
-//each of the Fields can be null. this is nom mandatory to add in info.
-//the Fields:
-
-//description : for caricurize more spesific the task.
-
-//priority(1-10):WHEN :
-//              1- if i fill board and have nothin else important to do.
-//              10- its a matter of life and death !
-
-//deadline: when does this task have to be done.
-
-//timeEstimated: Cumulative work days planned for this task
-
-//parentTask:This task is a subtask of the given parent task.
-
-let taskExtraInfo = {}
 
 //THE WORK WITH LOCAL STORAGE:
 
@@ -89,7 +40,6 @@ document.documentElement.className = localStorage.theme
 //initialize page:
 
 displayElements()
-
 //bottons events handaling installation
 
 document.getElementById('submit-add-to-do').addEventListener('click', handleAddToDoTask)
@@ -116,6 +66,7 @@ document.getElementById('doneTasks').addEventListener('mouseleave', mouseleavePa
 document.getElementById('toDoTasks').addEventListener('dblclick', handleDubleClick)
 document.getElementById('inProgressTasks').addEventListener('dblclick', handleDubleClick)
 document.getElementById('doneTasks').addEventListener('dblclick', handleDubleClick)
+
 
 //FUNCTIONS
 
