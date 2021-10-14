4de68b16ce5e3f2b4caefed5aa72c19491205a75
AVIV POLAK <avivp0437@gmail.com>
2021-10-14 08:43:20 +0300

    some more orgenize

--- solution/addEvents.js
+++ solution/addEvents.js
@@ -1,40 +1,40 @@
-// import {
-//     handleSearchKeyup,
-//     showExtraTodo,
-//     showExtraInProgress,
-//     showExtraDone,
-//     handleRemove,
-//     mouseOverParent,
-//     handleDubleClick,
-//     handleBlur,
-//     handleKeyDown,
-//     mouseleaveParent,
-// } from './events'
+import {
+    handleSearchKeyup,
+    showExtraTodo,
+    showExtraInProgress,
+    showExtraDone,
+    handleRemove,
+    mouseOverParent,
+    handleDubleClick,
+    mouseleaveParent,
+} from './events'
+import { handleAddToDoTask, handleaddInProgressTask, handleaddDoneTask } from './add'
+import { handleLoad, handleSave } from './api'
 
-// //bottons events handaling installation
+//bottons events handaling installation
+export function addEvents() {
+    document.getElementById('submit-add-to-do').addEventListener('click', handleAddToDoTask)
+    document.getElementById('submit-add-in-progress').addEventListener('click', handleaddInProgressTask)
+    document.getElementById('submit-add-done').addEventListener('click', handleaddDoneTask)
+    document.getElementById('todoAddInfo').addEventListener('click', showExtraTodo)
+    document.getElementById('inProgressAddInfo').addEventListener('click', showExtraInProgress)
+    document.getElementById('doneAddInfo').addEventListener('click', showExtraDone)
+    document.getElementById('inProgressTasks').addEventListener('click', handleRemove)
+    document.getElementById('toDoTasks').addEventListener('click', handleRemove)
+    document.getElementById('doneTasks').addEventListener('click', handleRemove)
+    document.getElementById('save-btn').addEventListener('click', handleSave)
+    document.getElementById('load-btn').addEventListener('click', handleLoad)
 
-// document.getElementById('submit-add-to-do').addEventListener('click', handleAddToDoTask)
-// document.getElementById('submit-add-in-progress').addEventListener('click', handleaddInProgressTask)
-// document.getElementById('submit-add-done').addEventListener('click', handleaddDoneTask)
-// document.getElementById('todoAddInfo').addEventListener('click', showExtraTodo)
-// document.getElementById('inProgressAddInfo').addEventListener('click', showExtraInProgress)
-// document.getElementById('doneAddInfo').addEventListener('click', showExtraDone)
-// document.getElementById('inProgressTasks').addEventListener('click', handleRemove)
-// document.getElementById('toDoTasks').addEventListener('click', handleRemove)
-// document.getElementById('doneTasks').addEventListener('click', handleRemove)
-// document.getElementById('save-btn').addEventListener('click', handleSave)
-// document.getElementById('load-btn').addEventListener('click', handleLoad)
+    // task events handaling installation
 
-// // task events handaling installation
-
-// document.getElementById('search').addEventListener('keyup', handleSearchKeyup)
-// document.getElementById('toDoTasks').addEventListener('mouseover', mouseOverParent)
-// document.getElementById('inProgressTasks').addEventListener('mouseover', mouseOverParent)
-// document.getElementById('doneTasks').addEventListener('mouseover', mouseOverParent)
-// document.getElementById('toDoTasks').addEventListener('mouseleave', mouseleaveParent)
-// document.getElementById('inProgressTasks').addEventListener('mouseleave', mouseleaveParent)
-// document.getElementById('doneTasks').addEventListener('mouseleave', mouseleaveParent)
-// document.getElementById('toDoTasks').addEventListener('dblclick', handleDubleClick)
-// document.getElementById('inProgressTasks').addEventListener('dblclick', handleDubleClick)
-// document.getElementById('doneTasks').addEventListener('dblclick', handleDubleClick)
-// console.log(document.getElementById('doneTasks'))
+    document.getElementById('search').addEventListener('keyup', handleSearchKeyup)
+    document.getElementById('toDoTasks').addEventListener('mouseover', mouseOverParent)
+    document.getElementById('inProgressTasks').addEventListener('mouseover', mouseOverParent)
+    document.getElementById('doneTasks').addEventListener('mouseover', mouseOverParent)
+    document.getElementById('toDoTasks').addEventListener('mouseleave', mouseleaveParent)
+    document.getElementById('inProgressTasks').addEventListener('mouseleave', mouseleaveParent)
+    document.getElementById('doneTasks').addEventListener('mouseleave', mouseleaveParent)
+    document.getElementById('toDoTasks').addEventListener('dblclick', handleDubleClick)
+    document.getElementById('inProgressTasks').addEventListener('dblclick', handleDubleClick)
+    document.getElementById('doneTasks').addEventListener('dblclick', handleDubleClick)
+}
--- solution/dataStructures.js
+++ solution/dataStructures.js
@@ -1,3 +1,4 @@
+import { setTheme } from './dom'
 //DATA STRUCTURES-
 
 //***"tasks" :
